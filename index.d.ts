1f68e2cb47717f2e97c8f4f641bc761fc4c7941b
Wasiq Kashkari <w.kashkari@outlook.com>
2020-05-22 15:29:12 +0200

    PA-4892: Return RedisClient not IORedis.Redis

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module "@rplan/redis-client" {
   import IORedis = require('ioredis')
-  export function createNewRedisClient(): IORedis.Redis
-  export function getRedisClient(): IORedis.Redis
+  export interface RedisClient extends IORedis.Redis {}
+  export function createNewRedisClient(): RedisClient
+  export function getRedisClient(): RedisClient
   export function disconnectRedisClient(): void
   export function shutdownRedisClient(): void
   export function resetShutdownForTests(): void
-  export interface RedisClient extends IORedis.Redis {}
 }
 
